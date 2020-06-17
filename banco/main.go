c1fce3739bbd95c6f69b062b0e61b64e5af2aa70
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-06-17 01:27:24 -0300

    Encapsulamento, ponteiros, múltiplos retornos

--- banco/main.go
+++ banco/main.go
@@ -1,26 +1,41 @@
 package main
 
-import "fmt"
-
-type ContaCorrente struct {
-	titular       string
-	numeroAgencia int
-	numeroConta   int
-	saldo         float64
-}
+import (
+	"curso-go-alura/banco/contas"
+	"fmt"
+)
 
 func main() {
 
-	contaDaMariana := ContaCorrente{
-		titular:       "Mariana",
-		numeroAgencia: 532,
-		numeroConta:   8963,
-		saldo:         563.3,
-	}
+	// contaDaMariana := ContaCorrente{
+	// 	titular:       "Mariana",
+	// 	numeroAgencia: 532,
+	// 	numeroConta:   8963,
+	// 	saldo:         563.3,
+	// }
 
-	contaDaBruna := ContaCorrente{
-		"Bruna", 222, 333, 100.3}
+	// contaDaBruna := ContaCorrente{
+	// 	"Bruna", 222, 333, 100.3}
 
-	fmt.Println(contaDaMariana, contaDaBruna)
+	// fmt.Println(contaDaMariana, contaDaBruna)
 
+	// var contadaMagali *ContaCorrente
+	// contadaMagali = new(ContaCorrente)
+	// contadaMagali.titular = "Magali"
+
+	// // Conteúdo da conta da Magali
+	// fmt.Println(*contadaMagali)
+
+	// // Endereço da conta da Magali
+	// fmt.Println(&contadaMagali)
+
+	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
+	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 100}
+
+	status := contaDaSilvia.Tranferir(200, &contaDoGustavo)
+
+	fmt.Println(status)
+
+	fmt.Println(contaDaSilvia)
+	fmt.Println(contaDoGustavo)
 }
