42263203b7b3664fb1defe252c621d96dbab3b47
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-06-18 01:35:18 -0300

    Iniciando aplicação web

---
+++ produtos/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Produto struct {
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+var temp = template.Must(template.ParseGlob("templates/*.html"))
+
+func main() {
+	http.HandleFunc("/", index)
+	http.ListenAndServe(":8000", nil)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	produtos := []Produto{
+		{"Tenis", "Confortavel", 199, 3},
+		{"Quadro", "dos Beatles", 100, 5},
+	}
+	temp.ExecuteTemplate(w, "Index", produtos)
+}
