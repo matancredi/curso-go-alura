aae11c555234be5ffab351bc5c8bdec87228ce9d
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-06-18 23:18:14 -0300

    Inserindo novo produto

--- produtos/controllers/produtos.go
+++ produtos/controllers/produtos.go
@@ -3,7 +3,9 @@ package controllers
 import (
 	"curso-go-alura/produtos/models"
 	"html/template"
+	"log"
 	"net/http"
+	"strconv"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
@@ -11,4 +13,30 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	produtos := models.BuscaTodosOsProdutos()
 	temp.ExecuteTemplate(w, "Index", produtos)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		preco := r.FormValue("preco")
+		quantidade := r.FormValue("quantidade")
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+		if err != nil {
+			log.Println("Erro na conversão do preço:", err)
+		}
+
+		quantidadeConvertida, err := strconv.Atoi(quantidade)
+		if err != nil {
+			log.Println("Erro na conversão da quantidade:", err)
+		}
+
+		models.CriarNovoProduto(nome, descricao, precoConvertido, quantidadeConvertida)
+	}
+	http.Redirect(w, r, "/", 301)
 }
