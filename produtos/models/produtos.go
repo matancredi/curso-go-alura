ea73764dad3882286d5de7b3d8b389e7784516e6
Mariana Tancredi Alvares Fernandes <matancredi@hotmail.com>
2020-06-18 23:49:00 -0300

    Removendo produto

--- produtos/models/produtos.go
+++ produtos/models/produtos.go
@@ -33,6 +33,7 @@ func BuscaTodosOsProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -53,6 +54,18 @@ func CriarNovoProduto(nome, descricao string, preco float64, quantidade int) {
 	}
 
 	insereDados.Exec(nome, descricao, preco, quantidade)
+
+	defer db.Close()
+}
+
+func DeletarProduto(id string) {
+	db := db.ConectaBanco()
+
+	deletarProduto, err := db.Prepare("delete from produtos where id=$1")
+	if err != nil {
+		panic(err.Error())
+	}
+	deletarProduto.Exec(id)
 
 	defer db.Close()
 }
